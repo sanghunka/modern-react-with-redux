6bce2a7a42b17de82765cb6cab22af2fded7c47e
Sanghun Kang <sanghunka@gmail.com>
2022-07-21 15:07:41 +0900

    34. Passing Props - Solutions

--- components/src/index.js
+++ components/src/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { faker } from "@faker-js/faker";
-import CommentDetail from './CommentDetail'; // You don't have to put the actual file extension on there so long
+import CommentDetail from "./CommentDetail"; // You don't have to put the actual file extension on there so long
 
 const App = () => {
   return (
     <div className="ui container comments">
-        <CommentDetail author="sam" />
-        <CommentDetail author="Alex" />
-        <CommentDetail author="Jane" />
+      <CommentDetail
+        author="sam"
+        timeAgo="Today at 4:45PM"
+        content="Nice blog post"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 2:00AM"
+        content="I like the subject"
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Yesterday at 5:00PM"
+        content="I like the writing"
+        avatar={faker.image.avatar()}
+      />
     </div>
-  )
+  );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(<App />, document.querySelector("#root"));
